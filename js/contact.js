42ce6d40a0502660a8253851d2984517938ce755
tendaiwm77 <munyaradzit05@gmail.com>
2025-05-29 00:20:08 -0700

    Create contact.js

---
+++ js/contact.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.getElementById('menu-toggle');
+  const navLinks = document.getElementById('nav-links');
+
+  toggle.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
+});
