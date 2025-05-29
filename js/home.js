f2635bcbf83286a8758a7878aabc861ad740642d
tendaiwm77 <munyaradzit05@gmail.com>
2025-05-29 01:21:07 -0700

    Update home.js

--- js/home.js
+++ js/home.js
@@ -35,3 +35,61 @@ document.addEventListener("DOMContentLoaded", () => {
 
   fadeSections.forEach(section => observer.observe(section));
 });
+
+<!-- Other content above... -->
+
+<!-- 📌 Place this at the end of your HTML file -->
+<script>
+  const carousel = document.querySelector('.stats-carousel');
+  const dots = document.querySelectorAll('.dot');
+  const leftArrow = document.querySelector('.arrow.left');
+  const rightArrow = document.querySelector('.arrow.right');
+  const totalSlides = dots.length;
+  let currentSlide = 0;
+  let autoSlide;
+
+  function goToSlide(index) {
+    currentSlide = (index + totalSlides) % totalSlides;
+    carousel.style.transform = `translateX(-${currentSlide * 100}%)`;
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[currentSlide].classList.add('active');
+  }
+
+  function nextSlide() {
+    goToSlide(currentSlide + 1);
+  }
+
+  function prevSlide() {
+    goToSlide(currentSlide - 1);
+  }
+
+  rightArrow.addEventListener('click', () => {
+    nextSlide();
+    resetAutoSlide();
+  });
+
+  leftArrow.addEventListener('click', () => {
+    prevSlide();
+    resetAutoSlide();
+  });
+
+  dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+      goToSlide(index);
+      resetAutoSlide();
+    });
+  });
+
+  function startAutoSlide() {
+    autoSlide = setInterval(nextSlide, 4000);
+  }
+
+  function resetAutoSlide() {
+    clearInterval(autoSlide);
+    startAutoSlide();
+  }
+
+  // Init
+  goToSlide(0);
+  startAutoSlide();
+</script>
